d9d080fe18cf2b41d4973c15f21029f0d62e1d9c
gaurav <sharma02gaurav@gmail.com>
2020-11-22 04:55:14 +0530

    final regex fix

---
+++ README.md
@@ -0,0 +1,6 @@
+## Run
+Follow the steps to run the program
+- `npm install`
+- `npm start`
+- Access `GET http://localhost:3000/api/name-count` to write content to file
+- Access `GET http://localhost:3000/api/name-count/Oliver` to get the names count
--- src/utility/count.utility.ts
+++ src/utility/count.utility.ts
@@ -2,12 +2,6 @@ import {Payload} from '../models';
 import {writeFile} from 'fs';
 import {resolve} from 'path';
 
-// interface CountWordResponse {
-// 	count: number,
-// 	skippable: Array<string>,
-// 	filter: string,
-// };
-
 /**
  * find the name from the dataset
  * @param {String} name the name to find from the file
@@ -21,58 +15,25 @@ export const ReadNameUtility = async (
 	}
 }
 
-// const countWord = (
-// 	text: string,
-// 	word: string,
-// 	skippable: Array<string>,
-// 	names: Array<string>,
-// ): CountWordResponse => {
-// 	const tempText = text.replace(new RegExp(word, 'g'), ` ${word} `);
-// 	let count = 0;
-// 	const filter = tempText.split(' ').filter(x => !skippable.includes(x) && names.includes(x));
-// 	console.log(`iterable: ${filter.length}`);
-// 	for (const w of filter) {
-// 		if (w === word) {
-// 			count += 1;
-// 			skippable.push(w);
-// 		}
-// 	}
-// 	return {count, skippable, filter: filter.join(' ')};
-// }
 export const WriteNamesUtility = async (
 	names: Array<string>,
 	content: string
 ): Promise<Array<Payload>> => {
 	const namesObject: any = {};
 	names.map(key => namesObject[key] = 0);
-	// console.log(namesObject);
 	/**
 	 * logic to count names
 	 */
 	const start = new Date();
 	console.log(`started at ${start.getTime()}`);
-	// let s: Array<string> = [];
-	// let f = content;
-	// for (const word of names) {
-	// 	const {count, filter} = countWord(f, word, s, names);
-	// 	f = filter;
-	// 	console.log(`${word}: ${count}\nskippable: ${s.length}`)
-	// 	namesObject[word] = count;
-	// }
 	let writeString = '';
 	const words = content.split(' ').sort();
-	// let inset = 0;
 	for (const name of names.sort()) {
-		const found = words.find(x => x.match(new RegExp(`^('|"| |)*${name}(,|:|\\.|!|;|\\?|'| |)*$`, 'g')));
-		if (found) {
-			let count = 0;
-			for(const word of words) {
-				if (word.match(new RegExp(new RegExp(`^('|"| |)*${name}(,|:|\\.|!|;|\\?|'| |)*$`, 'g')))) {
-					count += 1;
-				}
-			}
-			namesObject[name] = count;
-			writeString += `${name}: ${count}\n`;
+		const found = words.filter(x => x.match(new RegExp(`\\b(${name})\\b`, 'g')));
+		if (found && found.length) {
+			namesObject[name] = found.length;
+			writeString += `${name}: ${found.length}\n`;
+			console.log(writeString);
 		}
 	}
 	writeFile(
